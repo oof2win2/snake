d235ee0244c5ed18ef1c3bc4ec0963157d923601
honza <honza.kocourek.parklane@gmail.com>
2020-07-07 17:37:36 +0200

    Initial Commit

---
+++ had/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  had
+//
+//  Created by Honza on 07/07/2020.
+//  Copyright © 2020 Honza. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
