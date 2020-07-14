21770785a5cb122a5b4db69e3d88466812ea4ca7
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-07-14 18:36:05 +0200

    added printArea() function to print area, added area which it can print and reserved some variables for future use

--- had/main.c
+++ had/main.c
@@ -2,14 +2,92 @@
 //  main.c
 //  had
 //
-//  Created by Honza on 07/07/2020.
+//  Created by Honza on 7/07/2020.
 //  Copyright © 2020 Honza. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "rlutil.h"
 
+/*
+ TODO::
+ -Change area from global array to local array, as more levels would fuck this up
+ */
+
+#pragma mark default/reserved variables
+//the default variables for width and height are default sizes of the Terminal app on Mac where this was coded
+//width of array available, everything unused gets set to 0
+#define WIDTH       80
+//height of array available, everything unused gets set to 0
+#define HEIGHT      24
+
+//in the definition of the array, we assume that 0 is used to increase visibility in most editors
+#define FREE        0       //free piece of land in game
+#define WALL        1       //wall
+#define W           WALL    //wall
+#define DOOR        2       //door to exit level
+#define OBJ_FROM    100
+#define OBJ_TO      199
+//  common things (walls, doors etc.) ->  00-09
+//  objects to collect                ->  10-99
+//  snake parts                       ->  100-119
+//      reserved variable names:
+//  a   = area
+//      a[i][j]     -   a[HEIGHT][WIDTH]
+//      a[i][j]     -   i is smaller than 24, width is smaller than 80 (size of default terminal window
+//  w   = width
+//  h   = height
+//  a[row][coloumn]
+
+
+//  a = area
+//  a[coloumn][row]
+//  program uses [HEIGHT][WIDTH] because double arrays when entered in (every array on 2nd level has a new line) means that the second must be rows and the first variable must be columns
+#pragma mark setting area
+int area[HEIGHT][WIDTH] = {
+    {W, W, W, W, W, W, W, W, W, W, W, W},
+    {W, 0, 0, W, 0, 0, 0, 0, 0, 0, 0, W},
+    {W, 0, 0, W, 0, 0, 0, 0, 0, W, 0, W},
+    {W, 0, 0, W, 0, 0, 0, 0, 0, W, 0, W},
+    {W, 0, 0, W, 0, W, W, 0, 0, W, 0, W},
+    {W, 0, 0, W, 0, 0, W, 0, 0, W, 0, W},
+    {W, 0, 0, 0, 0, 0, W, W, W, W, 0, W},
+    {W, 0, 0, 0, 0, W, W, 0, 0, W, 0, W},
+    {W, 0, 0, W, 0, 0, 0, 0, 0, 0, 0, W},
+    {W, 0, 0, W, 0, 0, 0, 0, 0, 0, 0, W},
+    {W, 0, 0, W, 0, 0, W, W, 0, 0, 0, W},
+    {W, W, W, W, W, W, W, W, W, W, W, W},
+};
+
+//  prints out the area and needs the WIDTH parameter because in memory, the numbers are one behind another so it can see how far it needs to go for a new line
+#pragma mark printArea() function
+void printArea(int a[][WIDTH], int w, int h) {
+    int c;  //current point going through
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            c = a[i][j];
+            if (c == WALL)
+                printf("# ");
+            else if (c == FREE)
+                printf("  ");
+            else
+                printf("? ");
+        }
+        printf("\n");
+    }
+}
+#pragma mark testing()
+void testing() {
+    printArea(area, 12, 12);
+}
+void run() {
+    
+}
+#pragma mark main()
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    testing();
+    
+    //run();
     return 0;
 }
